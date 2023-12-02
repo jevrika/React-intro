4574d61ba0986204758580b9f3a551889d94ce4f
Ērika Skubida <erikaskubida23@gmail.com>
2023-12-02 09:03:24 +0200

    add some comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+
 let id = 0;
+
 type Task = {
   id: number,
   task: string,
@@ -11,7 +13,7 @@ const ToDoList = () => {
   const [list, setList] = useState<Task[]>([]);
   const [input, setInput] = useState('');
 
-
+// pievieno tasku ar id, nosaukumu un complete statusu
   const addTask = (task: string) => {
     id += 1
     const newTask: Task = {
@@ -19,8 +21,10 @@ const ToDoList = () => {
       task,
       complete: false
     }
+
     // pievienot esošajiem taskiem jaunu tasku
     setList([...list, newTask])
+
     // iztīra inputu
     setInput('')
   }
@@ -38,20 +42,30 @@ const ToDoList = () => {
       }
     }))
   }
+// Atgriež visus taskus, kuriem nesakrīt id ar padoto id
+  const handleDeleteClick = (taskId:number) => {
+    const removeItem = list.filter((task) => {
+      return task.id !== taskId;
+    })
+
+    return setList(removeItem)
+  }
 
   return (
     <div className='input__wrapper'>
       <ul className='tasks__list'>
+        {/* iet cauri list masīvām un izvada sarakstā tasku */}
         {list.map((task) => (
-          <li className={`list__items ${task.complete === true ? 'completed' : ''}`}
-            key={task.id}>
+          <li 
+            className={`list__items ${task.complete === true ? 'completed' : ''}`} key={task.id} >
             {task.task}
             <input className='li__checkbox' type='checkbox' checked={task.complete} onChange={() => toggleComplete(task.id)} />
+            <img className='delete__button' src='/src/assets/delete-button.svg' alt='delete button' onClick={() => handleDeleteClick(task.id)}  />
           </li>
         ))}
       </ul>
 
-      <input className='input' type='text' value={input} placeholder='add your task' onChange={(e) => setInput(e.target.value)}></input>
+      <input className='input' type='text' value={input} placeholder='add your task' onChange={(e) => setInput(e.target.value)} ></input>
       <button className='add__btn' type='submit' onClick={() => addTask(input)} > Add Task!</button>
 
     </div>
